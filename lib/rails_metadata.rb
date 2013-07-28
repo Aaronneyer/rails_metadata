d7bad0c6e7e925b6d38d425897546832d2fab8a4
Aaron Neyer <aaronneyer@gmail.com>
2013-07-28 14:03:33 -0700

    Moved typecast logic out of the method
    Previously, every time you called `value=`, it would check the type that
    was passed to the `metadata` method. Now the `metadata` method defines a
    lambda which does the typecast.

--- lib/rails_metadata.rb
+++ lib/rails_metadata.rb
@@ -11,19 +11,19 @@ module RailsMetadata
         self.metadata = {} unless self.metadata.present?
         self.metadata[name]
       end
+      typecast = case type
+                 when nil then val
+                 when :boolean then lambda { |v| v.is_a?(String) ? v.empty? : !!v }
+                 when :string then lambda { |v| v.to_s }
+                 when :integer then lambda { |v| v.to_i }
+                 when :float then lambda { |v| v.to_f }
+                 when :array then lambda { |v| v.to_a }
+                 when :hash then lambda { |v| v.to_hash }
+                 else raise ArgumentError, "Invalid type(:boolean, :string, :integer, :float, :array, :hash)"
+                 end
       define_method(name.to_s + '=') do |val|
-        val = case type
-              when nil then val
-              when :boolean then !(val.nil? || val.empty? || val)
-              when :string then String(val)
-              when :integer then val.to_i
-              when :float then val.to_f
-              when :array then Array(val)
-              when :hash then Hash(val)
-              else raise ArgumentError, "Invalid type(:boolean, :string, :integer, :float, :array, :hash)"
-              end
         self.metadata = {} unless self.metadata.present?
-        self.metadata.merge!(name => val)
+        self.metadata.merge!(name => typecast.call(val))
       end
     end
   end
